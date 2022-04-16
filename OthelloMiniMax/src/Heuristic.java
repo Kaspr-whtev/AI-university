c6a6c095abc1a79ee3dcb2caf9223211a8ef0786
Kaspr-whtev <60173961+Kaspr-whtev@users.noreply.github.com>
2022-04-16 08:36:45 +0200

    final bug fixes+comments

--- OthelloMiniMax/src/Heuristic.java
+++ OthelloMiniMax/src/Heuristic.java
@@ -1,8 +1,8 @@
 public enum Heuristic {
-    PieceParity, //maximizes owned pieces, while minimizing the opponent's
+    PieceParity, //maximizes owned pieces, while minimizing the opponent's owned pieces
     StaticWeights, //assigns a utility value to each position on the board,
-    // calculates the total value based on self and opponents pieces utility
-    Mobility, //maximize the number of available moves and minimize opponent's moves
-    Corners //value corners owned, potential corners and discourages opponent's corners
+    // calculates the total based on self and the opponent's total utility
+    Mobility, //maximize the number of available and potential moves and minimize the opponent's moves
+    Corners //value corners owned and potential corners and discourages opponent's corners
 }
 
--- OthelloMiniMax/src/Player.java
+++ OthelloMiniMax/src/Player.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 
+//abstract class for both types of players
+//Othello.play takes 2 of these as input
 public abstract class Player {
     public String name = "p";
+    //symbol is assigned in Othello.play
+    //it will be either the char representing white or black
     public char symbol = 'p';
 
     public Player(String name){
@@ -17,8 +21,12 @@ public abstract class Player {
         this.symbol = symbol;
     }
 
+    //every child needs to override this
     public abstract int[] move(Othello game);
 
+    //ArrayList.contains wasn't working for me when the arraylist contained other collections (namely int[])
+    //so I wrote a replacement specifically for this
+    //it only checks the 0th and 1st index, because here I only create 2 element arrays
     public static boolean findArray(ArrayList<int[]> list, int[] arr){
         for (int[] a : list){
             if (a[0] == arr[0] && a[1] == arr[1])
